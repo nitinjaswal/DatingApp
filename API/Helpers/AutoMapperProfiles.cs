037bdd60358a9c25d8eadefef5050f5bf303bdc6
Nitin <nitinjas.chd@gmal.com>
2021-01-30 23:27:25 +0530

    End of Section 8

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,20 @@
+using API.DTOs;
+using API.Entities;
+using AutoMapper;
+using System.Linq;
+using API.Extensions;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>()
+            .ForMember(dest =>dest.PhotoUrl,opt=>opt.MapFrom(src =>
+                    src.Photos.FirstOrDefault(x =>x.IsMain).Url))
+            .ForMember(dest =>dest.Age,opt =>opt.MapFrom(src =>src.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
