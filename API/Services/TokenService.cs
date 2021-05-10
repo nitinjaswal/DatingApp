825dc7aa2b287764b595ebc1aee983eb7b37fe88
Nitin <nitinjas.chd@gmal.com>
2021-05-10 16:46:19 +0530

    End of section 16

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
+using System.Threading.Tasks;
 using API.Entities;
 using API.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
@@ -14,21 +17,27 @@ namespace API.Services
     {
         //SymmetricSecurityKey is a type of key wherein only 1 key is used to encrypt
         //and decrypt the electronic information.
+        private readonly UserManager<AppUser> _userManager;
 
         //Asysmmetric key : wherein 2 keys are used to encrypt and decrypt the information. 
         //Like public and private keys.
         private readonly SymmetricSecurityKey _key;
-        public TokenService(IConfiguration config)
+        public TokenService(IConfiguration config, UserManager<AppUser> userManager)
         {
+            _userManager = userManager;
             _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
         }
-        public string CreateToken(AppUser user)
+        public async Task<string> CreateToken(AppUser user)
         {
             var claims = new List<Claim>
             {
                 new Claim(JwtRegisteredClaimNames.NameId,user.Id.ToString()),
                 new Claim(JwtRegisteredClaimNames.UniqueName,user.UserName),
             };
+
+            var roles  = await _userManager.GetRolesAsync(user);
+
+            claims.AddRange(roles.Select(role =>new Claim(ClaimTypes.Role, role)));
 
             var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
             var tokenDescriptor = new SecurityTokenDescriptor
